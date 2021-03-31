f580e430b0ac8bf97794709faf05f252a6b39f94
jaremn <nathan.jaremko@pointclickcare.com>
2021-03-31 12:20:53 -0400

    Remove BaseTemplate

--- src/lib.rs
+++ src/lib.rs
@@ -9,20 +9,6 @@ pub use socket::*;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-#[derive(askama::Template, Clone, Debug, Default)]
-#[template(path = "base.html", escape = "none")]
-pub struct BaseTemplate {
-    pub title: String,
-    pub head: String,
-    pub body: String,
-}
-
-impl Template for BaseTemplate {
-    fn render(&self) -> Result<String> {
-        Ok(<Self as askama::Template>::render(&self)?)
-    }
-}
-
 #[cfg(test)]
 mod tests {
     #[test]
